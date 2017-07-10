c491807ee56321488f9e7bba794b8bc73a7dc999
nabusman <nabusman@gmail.com>
2017-07-10 14:42:34 -0700

    data prep

---
+++ code/load_data.py
@@ -0,0 +1,74 @@
+import os
+import shutil
+import json
+import cPickle as pickle
+from string import punctuation
+from random import choice
+
+import nltk
+import numpy as np
+from tqdm import tqdm
+from sklearn.feature_extraction.text import CountVectorizer
+
+
+data_path = '../data'
+reviews_path = os.path.join(data_path,'yelp_academic_dataset_review.json')
+
+reviews = []
+ratings = []
+
+# Stem reviews before saving them
+stemmer = nltk.stem.porter.PorterStemmer()
+with open(reviews_path) as review_file:
+  for line in tqdm(review_file):
+    # Load review text
+    review_json = json.loads(line)
+    review_text = review_json['text']
+    # Remove punctuation
+    review_text = ''.join([x for x in review_text if x not in punctuation])
+    # Tokenize and stem words and append to data
+    review_text = ' '.join([stemmer.stem(x) for x in nltk.word_tokenize(review_text)])
+    reviews.append(review_text)
+    # Set reviews to positive or negative
+    stars = review_json['stars']
+    if stars > 3:
+      ratings.append(classes[0])
+    elif stars < 3:
+      ratings.append(classes[1])
+
+with open(os.path.join(data_path,'data.pickle'), 'wb') as pickle_file:
+  pickle.dump(reviews, pickle_file)
+
+with open(os.path.join(data_path,'ratings.pickle'), 'wb') as pickle_file:
+  pickle.dump(ratings, pickle_file)
+
+count_vect = CountVectorizer(stop_words = 'english', max_features = 10000)
+train_counts = count_vect.fit_transform(reviews)
+vocab = count_vect.get_feature_names()
+
+with open(os.path.join(data_path,'vocabulary.pickle'), 'wb') as pickle_file:
+  pickle.dump(vocab, pickle_file)
+
+train_path = '../data/reviews/train'
+val_path = '../data/reviews/val'
+test_path = '../data/reviews/test'
+
+[os.makedirs(os.path.join(x,y)) for x in [train_path, val_path, test_path] for y in ['pos', 'neg'] if not os.path.exists(os.path.join(x,y))]
+
+# Train:Validation:Test ratio of 8:1:1
+train_val_test_ratio = [train_path] * 8 + [val_path] * 1 + [test_path] * 1
+data_destination = [choice(train_val_test_ratio) for _ in xrange(len(reviews))]
+file_name = 0
+
+for i in tqdm(xrange(len(reviews))):
+  # For every review and ratings create a matrix of W x V (w is # of words in review and V is the size of the vocabulary)
+  review = reviews[i]
+  rating = ratings[i]
+  matrix = []
+  for word in nltk.word_tokenize(review):
+    row = [0] * len(vocab)
+    if word in vocab:
+      row[vocab.index(word)] = 1
+    matrix.append(row)
+  np.save(os.path.join(data_destination[i], rating, 'review_' + str(file_name)), np.array(matrix))
+  file_name += 1
